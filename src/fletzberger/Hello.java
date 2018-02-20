c31195bd54a26b4dbb273cf7c231dcb4ef58d071
mfletzberger <mfletzberger@student.tgm.ac.at>
2018-02-20 12:41:11 +0100

    Test und Hello hinzugefügt

---
+++ src/fletzberger/Hello.java
@@ -0,0 +1,16 @@
+package fletzberger;
+
+public class Hello {
+	public static void hallosimpel() {
+		System.out.println("Hallo");
+	}
+	public static void hallonormal(String name) {
+		System.out.println("Hallo "+name+"!");
+	}
+	public static void hallohoeflich() {
+		System.out.println("Guten Tag!");
+	}
+	public static void hallohoeflichnormal(String name) {
+		System.out.println("Guten Tag"+name+"!");
+	}
+}
---
+++ src/fletzberger/Test.java
@@ -0,0 +1,12 @@
+package fletzberger;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Hello.hallosimpel();
+		Hello.hallonormal("Michi");
+		Hello.hallohoeflich();
+		Hello.hallohoeflichnormal("Herr Fletzberger");
+	}
+
+}
